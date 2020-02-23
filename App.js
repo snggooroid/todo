c912ecabb5ec0581306ae8e0aa5ac360f0b8daae
snggooroid <snggooroid@snggooroidui-MacBookPro.local>
2020-02-23 23:47:41 +0900

    almost complete

--- App.js
+++ App.js
@@ -1,19 +1,190 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput,Dimensions,Platform, ScrollView } from 'react-native';
+import { render } from 'react-dom';
+import ToDo from "./ToDo"
+import {AppLoading} from "expo"
+import uuidv1 from 'uuid/v1';
+const {height,width} = Dimensions.get("window");
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+export default class App extends React.Component {
+  state ={
+    newToDo:"",
+    loadedToDo : false,
+    toDos:{}
+  };
+  componentDidMount = () => {
+    this._loadToDo();
+  }
+  render(){
+    const { newToDo,loadedToDo,toDos } = this.state;
+    if(!loadedToDo){
+      return <AppLoading />;
+    }     
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content"/>
+        <Text style={styles.title}>TO DO</Text>
+        <View style={styles.card}>
+          <TextInput 
+            style={styles.input} 
+            placeholder={"newToDo"} 
+            value={newToDo} 
+            onChangeText={this._controllNewToDo} 
+            placeholderTextColor={"#999"} 
+            returnKeyType={"done"}
+            autoCorrect={false}
+            onSubmitEditing={this._addToDo}
+          />
+          <ScrollView contentContainerStyle={styles.toDos}>
+            {Object.values(toDos).map(toDo => 
+            <ToDo 
+              key={toDo.id} 
+              deleteToDo={this._deleteToDo} 
+              unCompleteToDo={this._unCompleteToDo}
+              completeToDo={this._completeToDo}
+              updateToDo={this._updateToDo}
+              {...toDo} 
+            />)}
+          </ScrollView>
+        </View>
+      </View>
+    );
+  }
+    _controllNewToDo = text => {
+      this.setState({
+        newToDo : text
+      });
+    };
+    _loadToDo = () => {
+      this.setState({
+        loadedToDo : true
+      })
+    };
+    _addToDo = () => {
+      const {newToDo} = this.state;
+      if(newToDo !== ""){
+        this.setState(prevState => {
+          const ID = uuidv1();
+          const newToDoObject = {
+            [ID] : {
+              id : ID,
+              isCompleted : false,
+              text : newToDo,
+              createAt : Date.now()
+            }
+          };
+          const newState = {
+            ...prevState,
+            newToDo:"",
+            toDos:{
+              ...prevState.toDos,
+              ...newToDoObject
+            }
+          };
+          return {...newState};
+        });
+      }
+    };
+    _deleteToDo = (id) => {
+      this.setState(prevState => {
+        const toDos = prevState.toDos;
+        delete toDos[id];
+        const newState = {
+          ...prevState,
+          ...toDos
+        }
+        return {...newState};
+      });
+    };
+    _unCompleteToDo = (id) => {
+      this.setState(prevState => {
+        const newState = {
+          ...prevState,
+          toDos:{
+            ...prevState.toDos,
+            [id] : {
+              ...prevState.toDos[id],
+              isCompleted:false
+            }
+          }
+        };
+        return {...newState};
+      });
+    };
+    _completeToDo = (id) => {
+      this.setState(prevState => {
+        const newState = {
+          ...prevState,
+          toDos:{
+            ...prevState.toDos,
+            [id] : {
+              ...prevState.toDos[id],
+              isCompleted:true
+            }
+          }
+        };
+        return {...newState};
+      });
+    };
+    _updateToDo = (id,text) => {
+      this.setState(prevState => {
+        const newState = {
+          ...prevState,
+          toDos:{
+            ...prevState.toDos,
+            [id] : {
+              ...prevState.toDos[id],
+              text:text
+            }
+          }
+        };
+        return {...newState};
+      });
+    };
+  } 
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F23657',
     alignItems: 'center',
     justifyContent: 'center',
+  },title : {
+    color:"white",
+    fontSize:30,
+    marginTop:50,
+    fontWeight:"200",
+    marginBottom:30
+  },card:{
+    backgroundColor:"white",
+    flex:1,
+    width:width-25,
+    borderTopLeftRadius:10,
+    borderTopRightRadius:10,
+    ...Platform.select({
+      ios:{
+        shadowColor:"rgb(50,50,50)",
+        shadowOpacity:0.5,
+        shadowRadius:5,
+        shadowOffset:{
+          height:-1,
+          width:0
+        }
+      },
+      android:{
+        elevation:5
+      }
+    })
   },
+  input:{
+    padding:20,
+    borderBottomColor:"#bbb",
+    borderBottomWidth:1,
+    fontSize:25
+  },
+  toDos:{
+    alignItems:"center"
+  }
 });
